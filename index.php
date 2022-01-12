eb276574fea14740e31a977f8673eccdcc9062e7
KGROLLEMUND <kyllian.grollemund@ynov.com>
2022-01-12 13:37:41 +0100

    test

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
-    $mysqli = new mysqli("localhost", "root", "root", "php_exam"); // Connexion à la db "php_exam"
-    // si vous avez une erreur ici, remplacez le deuxième "root" par une string vide
     
-    $result = $mysqli->query("SELECT * FROM users"); // On utilise l'instance créée pour faire une requête bidon
-    ?>
 
     $hello = "World";
 
---
+++ signup.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'includes/head.php'; ?>
+<body>
+    
+    <form>
+    <div class="mb-3">
+        <label for="InputPseudo" class="form-label">Pseudo</label>
+        <input type="text" class="form-text">
+    </div>
+    <div class="mb-3">
+        <label for="InputNom" class="form-label">Nom</label>
+        <input type="text" class="form-text">
+    </div>
+    <div class="mb-3">
+        <label for="InputPrenom" class="form-label">Prenom</label>
+        <input type="text" class="form-text">
+    </div>
+    <div class="mb-3">
+        <label for="InputEmail" class="form-label">Email</label>
+        <input type="text" class="form-text">
+    </div>
+    <div class="mb-3">
+        <label for="InputPassword" class="form-label">Password</label>
+        <input type="password" class="form-text">
+    </div>
+    
+    <button type="submit" class="btn btn-primary">S'inscrire</button>
+    </form>
+
+</body>
+</html>
